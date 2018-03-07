6855f6b4bae6ebfad55bcc3f4ae9c48bf6f93567
Nate Bucher <n8bizall@yahoo.com>
2018-03-07 10:21:17 -0500

    first commit

---
+++ CustomerProject/Migrations/201803061958224_initialization.cs
@@ -0,0 +1,29 @@
+namespace CustomerProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialization : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Sales = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Active = c.Boolean(nullable: false),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
